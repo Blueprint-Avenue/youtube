14a86c9befeccfd57139e5f0f99bdb67cebed6dd
Samiya <skmcintosh20@gmail.com>
2022-09-14 12:16:37 -0700

    update/created redux store and utils

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
+import {clearVideos} from "../store";
+import {getHomePageMovies} from "../store/reducers/getHomePageMovies";
 
 const Home = () => {
+	const dispatch = useDispatch();
+	const videos = useSelector((state) => state.youtubeApp.videos);
+
+	useEffect(() => {
+		return () => {
+			dispatch(clearVideos());
+		};
+	}, []);
+	useEffect(() => {
+		dispatch(getHomePageMovies());
+	}, []);
+
 	return (
 		<div className="max-h-screen overflow-hidden">
 			<div style={{height: "5.5vh"}}>
