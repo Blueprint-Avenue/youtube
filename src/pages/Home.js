2eec37292ffd35251bf353b4809e7840da4bd882
Samiya <skmcintosh20@gmail.com>
2022-09-14 11:30:15 -0700

    completed sidebar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 
 const Home = () => {
 	return (
-		<div className="">
-			<Navbar />
+		<div className="max-h-screen overflow-hidden">
+			<div style={{height: "5.5vh"}}>
+				<Navbar />
+			</div>
+			<div className="flex" style={{height: "94.5vh"}}>
+				<Sidebar />
+			</div>
 		</div>
 	);
 };
